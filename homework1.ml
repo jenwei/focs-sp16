3aed53ec6b6030cb17da988c776c5fb3a0dc0382
Jennifer Wei <jennifer.wei@students.olin.edu>
2016-01-24 17:11:33 -0500

    UPDATE: Q1 complete!

--- homework1.ml
+++ homework1.ml
@@ -12,13 +12,13 @@ Remarks, if any:
 
 
 (*
-* lease fill in this file with your solutions and submit it
+* Please fill in this file with your solutions and submit it
 *
-T  unctions below are stubs that you should replace with your
-* ow  iplementation.
+The functions below are stubs that you should replace with your
+* own implementation.
 *
-* Aw a  ake sure you can #use this file before submitting it.
-* It ha  t load without any errors.
+* Always make sure you can #use this file before submitting it.
+* It has to load without any errors.
 *
 *)
 
@@ -27,7 +27,7 @@ T  unctions below are stubs that you should replace with your
 (* Question 1 *)
 
 let rec gcd (a,b) = 
-  (*usi   the Euclidean algorithm mentioned in class*)
+  (*using the Euclidean algorithm mentioned in class*)
   if b == 0 then a 
   else gcd (b,a mod b);;
 
@@ -45,79 +45,92 @@ let is_coprime (a,b) =
 (**
    is_coprime (1,2);; (*true*)
    is_coprime (2,3);; (*true*)
-   is_coprime (10,20);;  (*false*)
+   is_coprime (10,20);;     (*false*)
    is_coprime (16,8);; (*false*)
  **)
 
 let euler_inner (n,x) = 
   if x == 1 then x
-  else if is_coprime(n,x) then euler_inner(n,x-1) + 1 
+  else if is_coprime (n,x) then euler_inner (n,x-1) + 1 
   else euler_inner (n,x-1);;
 
 let euler (n) = euler_inner (n,n);;
 
 (* euler test *)
-euler (1);; (*1*)
-euler (2);; (*1*)
-euler (3);; (*2*)
-euler (4);; (*2*)
-euler (10);; (*4*)
-euler (20);; (*8*)
-euler (5555);; (*4000*)
+(**
+   euler (1);; (*1*)
+   euler (2);; (*1*)
+   eule   r (3);; (*2*)
+   euler (4);; (*2*)
+   euler (10);; (*4*)
+   euler (20);; (*8*)
+   euler (5555);; (*4000*)
+ **)
 
-let coprimes_inner (n,x) = 
-  if x == 1 then [x] 
-  else 
-  if is_coprime(n,x) then [x] 
-  else coprimes_inner(n,x-1);;
+let rec coprimes_inner (n,x) = 
+  (*http://stackoverflow.com/questions/6732524/what-is-the-easiest-way-to-add-an-element-to-the-end-of-the-list*)
+  if x == 1 then [x]
+  else if is_coprime (n,x) then coprimes_inner (n,x-1) @ [x]
+  else coprimes_inner (n,x-1);;
 
 let coprimes (n) = coprimes_inner (n,n);;
 
 (* coprimes test *)
+(**
+   coprimes (1);;
+   coprimes (2);;
+   coprimes (3);;
+   coprimes (4);;
+   coprimes (10);;
+   coprimes (20);;
+   coprimes (5555);;
+ **)
+
 
 
 (* Question 2 *)
 
-let append ,ys) = 
-failwith "not implemented"
-
-
-let fl   attexss) = 
-failwith "not implemented"
-
-
-let nth    (s) = 
+let append (xs,ys) = 
   failwith "not implemented"
 
 
-let last (xs) =ailwith "not implemented"
+let flatten (xss) = 
+  failwith "not implemented"
+
+
+let nth (n,xs) = 
+  failwith "not implemented"
+
+
+let last (xs) = 
+  failwith "not implemented"
 
 
 let separate (xs) = 
-  failwith "not impleted"
+  failwith "not implemented"
 
 
 
 (* Question 3 *)
 
 let setIn (e,xs) = 
-  failw "not implemented"
+  failwith "not implemented"
 
 
 let setSub (xs,ys) = 
-  failwith " implemented"
+  failwith "not implemented"
 
 
 let setEqual (xs,ys) = 
-  failwith "notplemented"
+  failwith "not implemented"
 
 
 let setUnion (xs,ys) = 
-  failwith "not immented"
+  failwith "not implemented"
 
 
 let setInter (xs,ys) = 
-  failwith "not iemented"
+  failwith "not implemented"
 
 
 let setSize (xs) = 

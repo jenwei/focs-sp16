6d0ce6e2bd584c225b695e7c86a4e5ab94e35a76
Jennifer Wei <jennifer.wei@students.olin.edu>
2016-01-24 15:56:02 -0500

    UPDATE: first few parts of Q1
    stuck on coprimes

--- homework1.ml
+++ homework1.ml
@@ -12,83 +12,114 @@ Remarks, if any:
 
 
 (*
- *
- * Please fill in this file with your solutions and submit it
- *
- * The functions below are stubs that you should replace with your
- * own implementation.
- *
- * Always make sure you can #use this file before submitting it.
- * It has to load without any errors.
- *
- *)
+* lease fill in this file with your solutions and submit it
+*
+T  unctions below are stubs that you should replace with your
+* ow  iplementation.
+*
+* Aw a  ake sure you can #use this file before submitting it.
+* It ha  t load without any errors.
+*
+*)
 
 
 
 (* Question 1 *)
 
-let gcd (a,b) = 
-   failwith "not implemented"
+let rec gcd (a,b) = 
+  (*usi   the Euclidean algorithm mentioned in class*)
+  if b == 0 then a 
+  else gcd (b,a mod b);;
 
+(* gcd test *)
+(**
+   gcd (3,9);; (*3*)
+   gcd (4,2);; (*2*)
+   gcd (60,70);; (*10*)
+ **)
 
 let is_coprime (a,b) = 
-   failwith "not implemented"
+  gcd (a,b) == 1;;
 
+(* is_coprime test *)
+(**
+   is_coprime (1,2);; (*true*)
+   is_coprime (2,3);; (*true*)
+   is_coprime (10,20);;  (*false*)
+   is_coprime (16,8);; (*false*)
+ **)
 
-let euler (n) = 
-   failwith "not implemented"
+let euler_inner (n,x) = 
+  if x == 1 then x
+  else if is_coprime(n,x) then euler_inner(n,x-1) + 1 
+  else euler_inner (n,x-1);;
 
+let euler (n) = euler_inner (n,n);;
 
-let coprimes (n) = 
-   failwith "not implemented"
+(* euler test *)
+euler (1);; (*1*)
+euler (2);; (*1*)
+euler (3);; (*2*)
+euler (4);; (*2*)
+euler (10);; (*4*)
+euler (20);; (*8*)
+euler (5555);; (*4000*)
 
+let coprimes_inner (n,x) = 
+  if x == 1 then [x] 
+  else 
+  if is_coprime(n,x) then [x] 
+  else coprimes_inner(n,x-1);;
+
+let coprimes (n) = coprimes_inner (n,n);;
+
+(* coprimes test *)
 
 
 (* Question 2 *)
 
-let append (xs,ys) = 
-   failwith "not implemented"
+let append ,ys) = 
+failwith "not implemented"
 
 
-let flatten (xss) = 
-   failwith "not implemented"
+let fl   attexss) = 
+failwith "not implemented"
 
 
-let nth (n,xs) = 
-   failwith "not implemented"
+let nth    (s) = 
+  failwith "not implemented"
 
 
-let last (xs) = 
-   failwith "not implemented"
+let last (xs) =ailwith "not implemented"
 
 
 let separate (xs) = 
-   failwith "not implemented"
+  failwith "not impleted"
 
 
 
 (* Question 3 *)
 
 let setIn (e,xs) = 
-   failwith "not implemented"
+  failw "not implemented"
 
 
 let setSub (xs,ys) = 
-   failwith "not implemented"
+  failwith " implemented"
 
 
 let setEqual (xs,ys) = 
-   failwith "not implemented"
+  failwith "notplemented"
 
 
 let setUnion (xs,ys) = 
-   failwith "not implemented"
+  failwith "not immented"
 
 
 let setInter (xs,ys) = 
-   failwith "not implemented"
+  failwith "not iemented"
 
 
 let setSize (xs) = 
-   failwith "not implemented"
+  failwith "not implemented"
 

cb8d42e6fb302f5cd10d94761611c2f558031999
Jennifer Wei <jennifer.wei@students.olin.edu>
2016-02-16 01:24:55 -0500

    UPDATE: name add to homework 4

--- homework4.ml
+++ homework4.ml
@@ -1,47 +1,47 @@
 (*************************************************** 
 
-HOMEWORK 4
+   HOMEWORK 4
 
-Name: 
+   Name: Jennifer Wei
 
-Email:
+   Email: jennifer.wei@students.olin.edu
 
-Remarks, if any:
+   Remarks, if any:
 
-***************************************************)
+ ***************************************************)
 
 
 
 (*
- *
- * Please fill in this file with your solutions and submit it
- *
- * The functions below are stubs that you should replace with your
- * own implementation.
- *
- * Always make sure you can #use this file before submitting it.
- * Do that in a _fresh_ OCaml shell 
- * It has to load without any errors.
- *
- *)
+*
+* Please fill in this file with your solutions and submit it
+*
+* The functions below are stubs that you should replace with your
+* own implementation.
+*
+* Always make sure you can #use this file before submitting it.
+* Do that in a _fresh_ OCaml shell 
+* It has to load without any errors.
+*
+*)
 
 
 (* 
- * String <-> characters utility functions:
- *
- *   explode : string -> char list
- *      returns the list of characters making up a string
- *
- *   implode : char list -> string
- *      concatenates the list of characters into a string
- *
- *)
+* String <-> characters utility functions:
+*
+*   explode : string -> char list
+*      returns the list of characters making up a string
+*
+*   implode : char list -> string
+*      concatenates the list of characters into a string
+*
+*)
 
 let explode str = 
   let rec acc index result = 
     if (index<0) then result
     else acc (index-1) ((String.get str index)::result) in
-  acc (String.length(str)-1) []
+    acc (String.length(str)-1) []
 
 let implode cs = 
   List.fold_right (fun a r -> (String.make 1 a)^r) cs ""
@@ -49,37 +49,37 @@ let implode cs =
 
 
 (*
- *  The type of a DETERMINISTIC finite automaton
- * 
- *  Note that the delta here is _actually_ a function
- *  from states and symbols to states
- * 
- *)
+*  The type of a DETERMINISTIC finite automaton
+* 
+*  Note that the delta here is _actually_ a function
+*  from states and symbols to states
+* 
+*)
 
 type 'a dfa = { states: 'a list;
-		alphabet: char list;
-		delta: 'a -> char -> 'a;
-		start : 'a;
-		accepting : 'a list }
+                		alphabet: char list;
+                		delta: 'a -> char -> 'a;
+                		start : 'a;
+                		accepting : 'a list }
 
 
 (*
- *  A sample DFA that accepts all strings over
- *  {a,b} with a multiple-of-3 number of a's
- *
- *)
+*  A sample DFA that accepts all strings over
+*  {a,b} with a multiple-of-3 number of a's
+*
+*)
 
 let dfaThreeA = { 
   states = ["start";"one";"two"];
   alphabet = ['a';'b'];
   delta = (fun q a -> 
-             match (q,a) with
-	       ("start",'a') -> "one"
-	     | ("one",'a') -> "two"
-	     | ("two",'a') -> "start"
-	     | ("start",'b') -> "start"
-	     | ("one",'b') -> "one"
-	     | ("two",'b') -> "two");
+            match (q,a) with
+                	       ("start",'a') -> "one"
+              	     | ("one",'a') -> "two"
+              	     | ("two",'a') -> "start"
+              	     | ("start",'b') -> "start"
+              	     | ("one",'b') -> "one"
+              	     | ("two",'b') -> "two");
   start = "start";
   accepting = ["start"]
 } 
@@ -105,42 +105,42 @@ let acceptDFA dfa input = failwith "acceptDFA not implemented"
  * finite automaton.
  *
  * This is basically the same as in the last homework
- *)
+*)
 
 let langDFA dfa n = 
   let rec expt a n = if n <= 0 then 1 else a*(expt a (n-1)) in
   let rec take n default l = 
     if n <= 0 then []
     else (match l with
-          | [] -> default::(take (n-1) default l)
-          | x::xs -> x::(take (n-1) default xs)) in
+           | [] -> default::(take (n-1) default l)
+           | x::xs -> x::(take (n-1) default xs)) in
   let to_base_n base size n = 
     let rec loop n = 
       if n <= 0 then []
       else if n mod base = 0 then 0::(loop (n / base))
       else (n mod base)::(loop ((n - n mod base) / base))  in
-    take size 0 (loop n)  in
+      take size 0 (loop n)  in
   let to_string alphabet size n = 
     let base = List.length alphabet in
     let num_base = to_base_n base size n in
-    implode (List.map (fun i -> List.nth alphabet i) num_base) in
-  if n < 0 then ()
-  else
-    let print_str s = if s = "" then print_string "  <epsilon>\n"
-                      else print_string ("  "^s^"\n")  in
-    let rec loop i = 
-      if i <= n then 
-	let ts = to_string dfa.alphabet i  in
-  	let bound = expt (List.length dfa.alphabet) i in
-  	let rec loop2 j = 
-  	  if j < bound then (if acceptDFA dfa (ts j) 
+      implode (List.map (fun i -> List.nth alphabet i) num_base) in
+    if n < 0 then ()
+    else
+      let print_str s = if s = "" then print_string "  <epsilon>\n"
+        else print_string ("  "^s^"\n")  in
+      let rec loop i = 
+        if i <= n then 
+          	let ts = to_string dfa.alphabet i  in
+          let bound = expt (List.length dfa.alphabet) i in
+          let rec loop2 j = 
+            if j < bound then (if acceptDFA dfa (ts j) 
                                then print_str (ts j)
-                             else ();
-  			     loop2 (j+1))
-  	  else ()  in
-  	(loop2 0; loop (i+1))
-      else ()  in
-      loop 0
+                               else ();
+                               loop2 (j+1))
+            else ()  in
+            (loop2 0; loop (i+1))
+        else ()  in
+        loop 0
 
 
 

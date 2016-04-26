e30257ba3c3405d521f79c7febd6a0c4bc45c5b0
Jennifer Wei <jennifer.wei@students.olin.edu>
2016-04-25 21:26:37 -0400

    UPDATE: name
    0.000000001% progress

--- homework9.ml
+++ homework9.ml
@@ -2,9 +2,9 @@
 
 HOMEWORK 9
 
-Name: 
+Name: Jennifer Wei
 
-Email:
+Email: jennifer.wei@students.olin.edu
 
 Remarks, if any:
 
@@ -12,17 +12,17 @@ Remarks, if any:
 
 
 (*
- *
- * Please fill in this file with your solutions and submit it
- *
- * The functions below are stubs that you should replace with your
- * own implementation.
- *
- * Always make sure you can #use this file before submitting it.
- * Do that in a _fresh_ OCaml shell 
- * It has to load without any errors.
- *
- *)
+*
+* Please fill in this file with your solutions and submit it
+*
+* The functions below are stubs that you should replace with your
+* own implementation.
+*
+* Always make sure you can #use this file before submitting it.
+* Do that in a _fresh_ OCaml shell 
+* It has to load without any errors.
+*
+*)
 
 
 
@@ -40,82 +40,82 @@ Remarks, if any:
  *
  * You don't need to know anything about this code -- you will
  * use functions fby, head, and tail described below instead
- *)
+*)
 
 module AbsStream :
-  sig
-      type 'a stream 
-      val mk : 'a -> (unit -> 'a stream) -> 'a stream 
-      val unmk1 : 'a stream -> 'a 
-      val unmk2 : 'a stream -> 'a stream
-      val cst : 'a -> 'a stream
-      val fby : 'a stream -> (unit -> 'a stream) -> 'a stream
-      val map : ('a -> 'b) -> 'a stream -> 'b stream
-      val filter : ('a -> 'b -> bool) -> 'a stream -> 'b stream -> 'b stream
-      val split : 'a stream -> ('a stream * 'a stream)
-      val zip : 'a stream -> 'b stream -> ('a * 'b) stream
-      val prefix : int -> 'a stream -> 'a list
-      val nth : int -> 'a stream -> 'a
-    end = 
-  struct
-    
-    type 'a stream = R of 'a * (unit -> 'a stream)
-	  
-    let memoize f = 
-      let memoized = ref None in
-      let new_f () = 
-	match !memoized with
-	| None -> let result = f () in memoized := Some result; result
-	| Some v -> v   in
+sig
+  type 'a stream 
+  val mk : 'a -> (unit -> 'a stream) -> 'a stream 
+  val unmk1 : 'a stream -> 'a 
+  val unmk2 : 'a stream -> 'a stream
+  val cst : 'a -> 'a stream
+  val fby : 'a stream -> (unit -> 'a stream) -> 'a stream
+  val map : ('a -> 'b) -> 'a stream -> 'b stream
+  val filter : ('a -> 'b -> bool) -> 'a stream -> 'b stream -> 'b stream
+  val split : 'a stream -> ('a stream * 'a stream)
+  val zip : 'a stream -> 'b stream -> ('a * 'b) stream
+  val prefix : int -> 'a stream -> 'a list
+  val nth : int -> 'a stream -> 'a
+end = 
+struct
+
+  type 'a stream = R of 'a * (unit -> 'a stream)
+                             	  
+  let memoize f = 
+    let memoized = ref None in
+    let new_f () = 
+      	match !memoized with
+        	| None -> let result = f () in memoized := Some result; result
+        	| Some v -> v   in
       new_f
-	
-    let mk h t = R (h, memoize t) 
-    let unmk1 s = let R (h,t) = s in h
-    let unmk2 s = let R (h,t) = s in t ()
+        	
+  let mk h t = R (h, memoize t) 
+  let unmk1 s = let R (h,t) = s in h
+  let unmk2 s = let R (h,t) = s in t ()
 
-    let rec cst v = mk v (fun () -> cst v)
-    let fby s1 ps2 = mk (unmk1 s1) ps2
-    let rec map f s = mk (f (unmk1 s)) (fun () -> map f (unmk2 s))
-    let rec filter p ctl s = if p (unmk1 ctl) (unmk1 s) then mk (unmk1 s) (fun () -> filter p (unmk2 ctl) (unmk2 s)) else filter p (unmk2 ctl) (unmk2 s)
-    let split s = (cst (unmk1 s), unmk2 s)
-    let rec zip s1 s2 = mk (unmk1 s1, unmk1 s2) (fun () -> zip (unmk2 s1) (unmk2 s2))
+  let rec cst v = mk v (fun () -> cst v)
+  let fby s1 ps2 = mk (unmk1 s1) ps2
+  let rec map f s = mk (f (unmk1 s)) (fun () -> map f (unmk2 s))
+  let rec filter p ctl s = if p (unmk1 ctl) (unmk1 s) then mk (unmk1 s) (fun () -> filter p (unmk2 ctl) (unmk2 s)) else filter p (unmk2 ctl) (unmk2 s)
+  let split s = (cst (unmk1 s), unmk2 s)
+  let rec zip s1 s2 = mk (unmk1 s1, unmk1 s2) (fun () -> zip (unmk2 s1) (unmk2 s2))
 
-    let rec prefix n s = if n > 0 then (unmk1 s)::(prefix (n-1) (unmk2 s)) else []
-    let rec nth n s = if n > 0 then nth (n-1) (unmk2 s) else unmk1 s
+  let rec prefix n s = if n > 0 then (unmk1 s)::(prefix (n-1) (unmk2 s)) else []
+  let rec nth n s = if n > 0 then nth (n-1) (unmk2 s) else unmk1 s
 
-  end
+end
 
 
 (*
- * These are the stream functions you will use
- *
- *)
+* These are the stream functions you will use
+*
+*)
 
 type 'a stream = 'a AbsStream.stream
 
 let cst : 'a -> 'a stream = AbsStream.cst
-        (* constant *)
+(* constant *)
 
 let fby : 'a stream -> (unit -> 'a stream) -> 'a stream = AbsStream.fby
-        (* followed by *)
+(* followed by *)
 
 let map : ('a -> 'b) -> 'a stream -> 'b stream = AbsStream.map
-        (* map a function over a stream *)
+(* map a function over a stream *)
 
 let filter : ('a -> 'b -> bool) -> 'a stream -> 'b stream -> 'b stream = AbsStream.filter
-           (* filter a stream based on a control stream and a predicate *)
+(* filter a stream based on a control stream and a predicate *)
 
 let zip : 'a stream -> 'b stream -> ('a * 'b) stream = AbsStream.zip
-        (* zip two streams into a stream of pairs *)
+(* zip two streams into a stream of pairs *)
 
 let split : 'a stream -> ('a stream * 'a stream) = AbsStream.split
-          (* split a stream into two streams *)
+(* split a stream into two streams *)
 
 let prefix : int -> 'a stream -> 'a list = AbsStream.prefix
-           (* return the first n elements of a stream *)
+(* return the first n elements of a stream *)
 
 let nth : int -> 'a stream -> 'a = AbsStream.nth
-           (* return the nth element of a stream *)
+(* return the nth element of a stream *)
 
 
 
@@ -123,8 +123,8 @@ let nth : int -> 'a stream -> 'a = AbsStream.nth
 
 let nats =
   let rec natsF () = fby (cst 0)
-                         (fun () -> (map (fun x -> x+1) (natsF ()))) in
-  natsF ()
+                       (fun () -> (map (fun x -> x+1) (natsF ()))) in
+    natsF ()
 
 let evens = map (fun x -> 2*x) nats
 let odds = map (fun x -> x+1) evens
@@ -140,8 +140,8 @@ let ampl =
     else if d = 1 then (v+1,(1,m))
     else (v-1,(-1,m))  in
   let rec f () = fby (zip (cst 0) (cst (1,1)))
-                     (fun () -> map transf (f ())) in
-  map (fun (x,y) -> x) (f ())
+                   (fun () -> map transf (f ())) in
+    map (fun (x,y) -> x) (f ())
 
 
 (* streams of the form a0,a1,a2,a3... that are useful for
@@ -156,9 +156,9 @@ let s_b = tag "b"
 
 
 (* 
- * QUESTION 1 
- * 
- *)
+* QUESTION 1 
+* 
+*)
 
 let scale n s = failwith "not implemented"
 
@@ -175,16 +175,16 @@ let rec stutter s = failwith "not implemented"
 
 
 (*
- * QUESTION 2
- * 
- *)
+* QUESTION 2
+* 
+*)
 
 let rec arctan z = failwith "not implemented"
 
 (* PLACEHOLDER -- REPLACE WITH YOUR OWN DEFINITION *)
 
 let pi = cst (0)
-    
+
 let rec newton f df guess = failwith "not implemented"
 
 let derivative f x = failwith "not implemented"
@@ -193,9 +193,9 @@ let limit epsilon s = failwith "not implemented"
 
 
 (* 
- * QUESTION 3 
- * 
- *)
+* QUESTION 3 
+* 
+*)
 
 
 let rev_prefixes s = failwith "not implemented"
